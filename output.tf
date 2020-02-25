c344de57d522760941061ed076f38f5aac255139
Sergio Teixeira <sergiopintoteixeira@gmail.com>
2020-02-25 10:38:41 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,3 @@
-output "bucket_domain_name" {
-  value = aws_s3_bucket.b.*.bucket_domain_name
-}
-output "bucket_name" {
-  value = aws_s3_bucket.b.*.id
-}
-output "bucket_arn" {
-  value = aws_s3_bucket.b.*.arn
+output "bucket" {
+  value = [for bucket in aws_s3_bucket.bucket : bucket]
 }
