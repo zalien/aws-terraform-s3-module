67aa3c3c8b09d414edd17b852c4317c22a27985d
Sergio Teixeira <sergiopintoteixeira@gmail.com>
2020-02-25 10:23:30 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
-variable "bucket_name" {}
-variable "environment" {}
+variable "name" {
+  type = set(string)
+}
 variable "policy" { default = "" }
-variable "counter" { default = 1 }
+variable "acl" { default = "private" }
+variable "tags" {
+  type        = map
+  description = "Tags for S3 Buckets"
+  default     = {}
+}
